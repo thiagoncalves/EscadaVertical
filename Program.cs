e2eb58730023f9a65cdd3bd04cb63e7a33dc5a68
Thiago Gonçalves Novaes <thiago.novaes@bunzlbrasil.protcapad.com.br>
2020-05-20 18:33:09 -0300

    EscadaVertical

---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EscadaVertical
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string p1;
+            int tamanho;
+
+            Console.Write("Escreva uma palavra: ");
+            p1 = Console.ReadLine().ToUpper();
+            tamanho = p1.Length;
+
+            for (int i = 0; i <= tamanho; i++)
+                Console.WriteLine(p1.Substring(0,i));
+
+            Console.WriteLine("");
+
+            for (int i = 0; i <= tamanho; i++)
+                Console.WriteLine(p1.Substring(i));
+
+            Console.ReadLine();
+        }
+    }
+}
